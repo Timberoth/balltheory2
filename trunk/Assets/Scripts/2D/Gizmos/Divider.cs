b5ed2f0c774c9c01ea9b13a52d2e8733ccfae12e
consciousgamer@gmail.com <consciousgamer@gmail.com@66051942-5e76-db1e-b493-1734cf169384>
2011-03-02 05:07:47 +0000

    Implemented 1st pass of rest of the gizmos.  Only have to work more on Modulus.

--- trunk/Assets/Scripts/2D/Gizmos/Divider.cs
+++ trunk/Assets/Scripts/2D/Gizmos/Divider.cs
@@ -3,8 +3,30 @@ using System.Collections;
 
 public class Divider : Gizmo {
 				
+	public int divisor = 1;		
+	
 	public override void DoMathematicalOperation()
+	{		
+		ballCounter /= divisor;		
+	}
+	
+	// Divisor can only go from 1-9
+	public void incrementDivisor()
 	{
-		// Need to track divisor.  Not sure how to handle remainder values.
-	}	
+		divisor++;
+		
+		// Roll back to 1
+		if( divisor >= 10 )
+			divisor = 1;
+	}
+	
+	// Divisor can only go from 1-9
+	public void decrementMultiplier()
+	{		
+		divisor--;
+	
+		// Roll up to 9
+		if( divisor <= 0 )
+			divisor = 9;
+	}
 }
---
+++ trunk/Assets/Scripts/2D/Gizmos/Storage.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Storage : Gizmo {
+				
+	public override void DoMathematicalOperation()
+	{
+		// Do nothing because this gizmo is only used for storage.
+	}	
+}
