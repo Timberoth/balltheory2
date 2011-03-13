5de9d183a5f45a5de083106bd050dc9959c90565
consciousgamer@gmail.com <consciousgamer@gmail.com@66051942-5e76-db1e-b493-1734cf169384>
2011-03-13 18:03:04 +0000

    Repeater is working but still not counting down the iterations.

--- Assets/Scripts/2D/Gizmos/Repeater.cs
+++ Assets/Scripts/2D/Gizmos/Repeater.cs
@@ -19,7 +19,7 @@ public class Repeater : Gizmo {
 		GameManager gameManager = gameManagerObject.GetComponent<GameManager>();		
 		if( gameManager.repeaterOutputObject == null )
 		{
-			print("A Repeater was unable to get the RepeaterOutput reference from the GameManager.");	
+			print("[ERROR] A Repeater was unable to get the RepeaterOutput reference from the GameManager.");	
 		}
 		
 		Vector3 spawnPoint = gameObject.transform.position;
@@ -31,9 +31,11 @@ public class Repeater : Gizmo {
 		repeaterOutput = repeaterOutputObject.GetComponent<RepeaterOutput>();
 		if( repeaterOutput == null )
 		{
-			print("A Repeater was unable to get the RepeaterOutput reference");	
+			print("[ERROR] A Repeater was unable to get the RepeaterOutput reference");	
 		}
-		repeaterOutput.SetBallObject( ballObject );
+		
+		// Pass the input object reference to the RepeaterOutput.
+		//repeaterOutput.SetBallObject( ballObject );
 		
 		
 		// Get the modifier box reference
@@ -41,41 +43,37 @@ public class Repeater : Gizmo {
 		if( modifierBox == null )
 		{
 			print("[ERROR] This Multiplier does not have an child ModifierBox.");	
-		}
-		
+		}			
+	}		
+	
+	public override void OnCollisionEnter( Collision collision )
+	{
+		// Check that the collision is with a ball.
+		if( collision.gameObject.tag == "Ball" )			
+		{													
+			// Increment the ball counter for RepeaterOutput.
+			repeaterOutput.AddBalls(1);
 			
-		// For some reason we need to reset the modifier box text to 1 because it defaults to 0.
-		//modifierBox.Reset();
-				
-	}
-		
-	public override void DoMathematicalOperation()
-	{		
-		// Do nothing
+			// Destroy the ball that entered
+			Destroy( collision.gameObject );
+						
+			// Reset the processing timer.
+			processingTimer = autoProcessingTimer;
+			
+			hasBeenHit = true;
+		}		
 	}	
 	
 	
 	// Need specialized processing function.
 	public override IEnumerator BeginProcessing()
-    {		
+    {				
 		// If we're already processing don't do anything.
 		if( processing )
 		{			
 			yield break;
 		}
-		
-		// Don't do anything if the gizmo is empty
-		if( ballCounter == 0 )
-		{
-			// Play failure sound effect
-			
-			// Play failure particle effect
-			
-			// Play failure animation
-			
-			yield break;
-		}
-						
+				
 		// Mark that we've begun processing.
 		processing = true;		
 						
@@ -89,32 +87,13 @@ public class Repeater : Gizmo {
 		// Wait for a little bit
         yield return new WaitForSeconds(0.5f);
 		
-		// Used the Repeater Output marker to figure out where the balls will be spit out from.
-		// TODO IMPLEMENT THIS PROPERLY
-		ballSpawnPoint = gameObject.transform.position;					
-		ballSpawnPoint.y = ballSpawnPoint.y - 0.5f*ballSize.y - 0.5f*gizmoSize.y - 0.1f;			
-						
-		int ballsAtStart = ballCounter;
 		
-		// Begin spitting out the balls and decrement the ball counter until all the balls are gone.
-		for( int i = 0; i < ballsAtStart; i++ )
-		{				
-			// Instantiate ball at the bottom of the gizmo			
-			Instantiate( ballObject, ballSpawnPoint, Quaternion.identity );
-			
-			ballCounter--;
-			
-			UpdateBallCountText();
-			
-			// Play sound
-			
-			// Play particle
-			
-			// Wait for little bit before spitting out the next ball.
-			yield return new WaitForSeconds(0.5f);
-		}
+		// Call the BeginProcessing function for the RepeaterOutput.		
+		StartCoroutine(repeaterOutput.BeginProcessing());
+		
 				
 		ballCounter = 0;
-		processing = false;		
+		processing = false;
+		hasBeenHit = false;
     }	
 }
--- Assets/Scripts/2D/Gizmos/RepeaterOutput.cs
+++ Assets/Scripts/2D/Gizmos/RepeaterOutput.cs
@@ -1,24 +1,94 @@
 using UnityEngine;
 using System.Collections;
 
-public class RepeaterOutput : MonoBehaviour {
-	
-	private GameObject ballObject;
-	
-	// Use this for initialization
-	void Start () 
+public class RepeaterOutput : Gizmo 
+{	
+	// Add this number of balls to the RepeaterOutput
+	public void AddBalls( int num )
 	{
-	
+		ballCounter += num;
+		UpdateBallCountText();
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	public override void OnCollisionEnter( Collision collision )
 	{
+		// Don't handle collisions for the RepeaterOutput because
+		// all input is coming from the Repeater.
+	}	
 	
+	
+	void OnTriggerEnter (Collider other) 
+	{
+		// Place some animation to show the ball passing through the object.
 	}
 	
-	public void SetBallObject( GameObject ball )
+	public override void Update()
 	{
-		ballObject = ball;
+		// Don't do anything because the BeginProcessing function will be called from the Repeater.
 	}
+	
+	// Need specialized processing function.
+	public override IEnumerator BeginProcessing()
+    {				
+		// If we're already processing don't do anything.
+		if( processing )
+		{			
+			yield break;
+		}
+		
+		// Don't do anything if the gizmo is empty
+		if( ballCounter == 0 )
+		{
+			// Play failure sound effect
+			
+			// Play failure particle effect
+			
+			// Play failure animation
+			
+			yield break;
+		}
+						
+		// Mark that we've begun processing.
+		processing = true;		
+						
+		// Play processing animation
+		
+		// Play particles
+		
+		// Play sounds
+								
+		
+		// Wait for a little bit
+        yield return new WaitForSeconds(0.5f);
+		
+		// Used the Repeater Output marker to figure out where the balls will be spit out from.
+		// TODO IMPLEMENT THIS PROPERLY
+		ballSpawnPoint = gameObject.transform.position;					
+		ballSpawnPoint.y = ballSpawnPoint.y - 0.5f*ballSize.y - 0.5f*gizmoSize.y - 0.1f;			
+						
+		int ballsAtStart = ballCounter;
+		
+		// Begin spitting out the balls and decrement the ball counter until all the balls are gone.
+		for( int i = 0; i < ballsAtStart; i++ )
+		{				
+			// Instantiate ball at the bottom of the gizmo			
+			Instantiate( ballObject, ballSpawnPoint, Quaternion.identity );
+			
+			ballCounter--;
+			
+			UpdateBallCountText();
+			
+			// Play sound
+			
+			// Play particle
+			
+			// Wait for little bit before spitting out the next ball.
+			yield return new WaitForSeconds(0.5f);
+		}
+				
+		ballCounter = 0;
+		processing = false;
+		hasBeenHit = false;
+    }		
+	
 }
